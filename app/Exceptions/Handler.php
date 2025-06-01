82a4f1fd9436413463ddd081e8f44fa2e06c2738
SheenyKates <dagalasheenykates@gmail.com>
2025-06-01 21:55:11 +0800

    update

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+use Illuminate\Validation\ValidationException;
+
+class Handler extends ExceptionHandler
+{
+    protected $levels = [
+        // 'SomeException' => 'critical',
+    ];
+
+    protected $dontReport = [
+        //
+    ];
+
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+
+    public function register()
+    {
+        $this->reportable(function (Throwable $e) {
+            //
+        });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof ValidationException) {
+            return response()->json([
+                'message' => 'Validation failed',
+                'errors' => $exception->errors(),
+            ], 422);
+        }
+
+        return parent::render($request, $exception);
+    }
+}
