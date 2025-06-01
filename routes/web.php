fab2df085c6e79a4a9a9e982a01affa66770390d
SheenyKates <dagalasheenykates@gmail.com>
2025-06-01 20:44:44 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,44 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'guestindex');
-Route::view('/greeting', 'greeting'); 
-Route::view('/home', 'home');
-Route::view('/about', 'about');
-Route::view('/category', 'category');
-Route::view('/dish', 'dish');
-Route::view('/explore', 'explore');
-Route::view('/favorites', 'favorites');
-Route::view('/explore-category', 'explore-category');
-Route::view('/guestabout', 'guestabout');
+// Redirect Laravel routes to static HTML files in public/
+
+Route::get('/', function () {
+    return response()->file(public_path('index.html'));
+});
+
+Route::get('/greeting', function () {
+    return response()->file(public_path('greeting.html'));  // Replace with actual file if exists
+});
+
+Route::get('/home', function () {
+    return response()->file(public_path('home.html'));
+});
+
+Route::get('/about', function () {
+    return response()->file(public_path('about.html'));
+});
+
+Route::get('/category', function () {
+    return response()->file(public_path('category.html'));
+});
+
+Route::get('/dish', function () {
+    return response()->file(public_path('dish.html'));
+});
+
+Route::get('/explore', function () {
+    return response()->file(public_path('explore.html'));
+});
+
+Route::get('/favorites', function () {
+    return response()->file(public_path('favorites.html'));
+});
+
+Route::get('/explore-category', function () {
+    return response()->file(public_path('explore-category.html'));
+});
+
+Route::get('/guestabout', function () {
+    return response()->file(public_path('guestabout.html'));
+});
